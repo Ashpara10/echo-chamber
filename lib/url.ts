ec2f36d5f2d68baa032f756529030f2142464f1e
AshPara10 <ashwinparande1156610c@gmail.com>
2024-03-25 10:51:03 +0530

    url change3

--- lib/url.ts
+++ lib/url.ts
@@ -2,7 +2,7 @@ import { getCookie } from "cookies-next";
 
 let url: string =
   process?.env?.NODE_ENV === "production"
-    ? "https://echo-chamber-gold.vercel.app"
+    ? "https://sunflower-gold.vercel.app"
     : "http://localhost:3000";
 
 export const userID = getCookie("user");
