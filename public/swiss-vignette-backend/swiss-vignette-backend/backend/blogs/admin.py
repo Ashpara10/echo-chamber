fa7ac01cc659cf9a3b69114332e27e5a7e1efe01
AshPara10 <ashwinparande1156610c@gmail.com>
2024-05-27 15:24:25 +0530

    minor updates

--- public/swiss-vignette-backend/swiss-vignette-backend/backend/blogs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'permalink', 'created_at', 'updated_at')
-
-# Register your models here.
-admin.site.register(Blog, BlogAdmin)
--- public/swiss-vignette-backend/swiss-vignette-backend/backend/orderHandlerApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrderhandlerappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "orderHandlerApp"
